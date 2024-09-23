4f1296b5dfe00c52bc40a27a8ee1f86d140f351a
snvkgram <ramgood@gmail.com>
2024-09-23 15:48:16 +0000

    initial commit

---
+++ src/screens/index.js
@@ -0,0 +1,10 @@
+
+import PetCreate from "./pet_details/pet_createForm.js";
+import PetEdit from "./pet_details/pet_editForm.js";
+import PetView from "./pet_details/pet_view.js";
+import Pets from "./pet_table/pet_index.js";
+
+export {
+PetCreate, PetEdit, PetView, 
+Pets
+};
